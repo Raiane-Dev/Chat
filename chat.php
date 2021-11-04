1b4e7811cb90d936e7acc9eee5b8e6bf4b35608b
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-04 10:27:40 -0300

    Update chat.php

--- chat.php
+++ chat.php
@@ -5,8 +5,8 @@
     }
 
     $result = array();
-    $message = isset($_POST['message']) ? $_POST['message'] : null;
-    $from = isset($_POST['from']) ? $_POST['from'] : null;
+    $message = isset($_POST['message']) ? md5($_POST['message']) : null;
+    $from = isset($_POST['from']) ? md5($_POST['from']) : null;
 
     if(!empty($message) && !empty($from)){
         $sql = "INSERT INTO `chat` (`message`,`from`) VALUES ('".$message."','".$from."')";
